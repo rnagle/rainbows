5e227c5751a1b406c53f17cbe6512d23d4279e63
Ryan Nagle <rmnagle@gmail.com>
2015-06-30 19:33:14 -0500

    add a jquery plugin, refactor rainbows to use js protoypes

--- rainbows.js
+++ rainbows.js
@@ -1,16 +1,26 @@
-var Rainbows = function(container, speed, size) {
-    var $ = jQuery,
-        self = this;
+(function() {
+    var $ = jQuery;
 
-    self.wrap = function(target) {
-        var target = $(target),
+    /**
+     * Rainbows class
+     */
+    var Rainbows = function(container, speed, size) {
+        this.container = $(container);
+        this.speed = speed || 2;
+        this.size = size || 15;
+        return this.start();
+    };
+
+    Rainbows.prototype.wrap = function(target) {
+        var self = this,
+            target = $(target),
             new_target = $('<div>'),
             nodes = target.contents().clone();
 
         nodes.each(function() {
             if (this.nodeType == 3) { // text
                 var new_html = "";
-                    text = this.wholeText;
+                text = this.wholeText;
 
                 for (var i=0; i < text.length; i++) {
                     if (text[i] == ' ')
@@ -28,21 +38,33 @@ var Rainbows = function(container, speed, size) {
         return new_target.html();
     };
 
-    self.injectCSS = function(duration, size) {
-        duration = duration || 2;
-        size = size || 15;
-        style = "<style>";
-        animation = "animation: color-change " + duration + "s infinite; ";
-        style += container + " span.rainbow { ";
+    Rainbows.prototype.guid = function() {
+        var s4 = function() {
+            return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+        };
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+    };
+
+    Rainbows.prototype.generateCSS = function() {
+        var style = "<style>",
+            animation = "animation: color-change " + this.speed + "s infinite; ",
+            rainbow_id = 'rainbow-' + this.guid(),
+            data_attr = 'data-rainbow-id',
+            selector = this.container.get(0).tagName.toLocaleLowerCase() +
+                        '[' + data_attr + '="' + rainbow_id + '"]';
+
+        this.container.attr(data_attr, rainbow_id);
+
+        style += selector + " span.rainbow { ";
         style += animation;
         style += "-moz-" + animation;
         style += "-webkit-" + animation;
         style += "-ms-" + animation;
         style += "-o-" + animation;
         style += "} ";
-        for (x = 1; x <= size; x++) {
-            style += container + " span.rainbow:nth-child(" + size + "n + " + x;
-            delay = x*duration/size - duration;
+        for (x = 1; x <= this.size; x++) {
+            style += selector + " span.rainbow:nth-child(" + this.size + "n + " + x;
+            delay = x * this.speed / this.size - this.speed;
             delay = "animation-delay: " + delay + "s; ";
             style += ") { " + delay;
             style += "-moz-" + delay;
@@ -58,10 +80,49 @@ var Rainbows = function(container, speed, size) {
         style += "@-ms-" + keyframes;
         style += "@-o-" + keyframes;
         style += "</style>";
+
         return style;
     }
-    if (typeof container !== 'undefined')
-        $(container).html(self.wrap(container));
-        $(container).prepend(self.injectCSS(speed, size));
-    return self;
-};
+
+    Rainbows.prototype.start = function() {
+        if (this.container.length && typeof this.container.data('originalHTML') == 'undefined') {
+            this.container.data('originalHTML', this.container.html());
+            this.container.html(this.wrap(this.container));
+            this.container.prepend(this.generateCSS());
+        }
+        return this;
+    };
+
+    Rainbows.prototype.stop = function () {
+        this.container.html(this.container.data('originalHTML'));
+        this.container.removeData('originalHTML');
+        this.container.removeData('rainbows');
+        return this;
+    };
+
+    /**
+     * If it seems safe, place Rainbows in global scope.
+     */
+    if (typeof window.Rainbows == 'undefined')
+        window.Rainbows = Rainbows;
+
+    /**
+     * As a jQuery plugin
+     */
+    $.fn.rainbows = [];
+
+    $.fn.rainbows = function(options) {
+        var settings = $.extend({
+            speed: 2,
+            size: 15
+        }, (typeof options == 'object')? options : {});
+
+        return this.each(function(idx, el) {
+            if (typeof $(el).data('rainbows') == 'undefined')
+                $(el).data('rainbows', new Rainbows(el, settings.speed, settings.size));
+
+            if (['start', 'stop'].indexOf(options) >= 0)
+                $(el).data('rainbows')[options]();
+        });
+    };
+}());
